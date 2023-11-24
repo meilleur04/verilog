34baf9d406e6cdca1d95b6cd008d801335e7e792
Tony Sheng <132284762+1TonyS@users.noreply.github.com>
2023-11-24 15:43:53 -0500

    Update control_game.v

--- control_game.v
+++ control_game.v
@@ -1,70 +1,81 @@
 module control_game(
-	input Clock, Resetn, Enable1Frame, start, move, win, done1, done2, done1_new, done2_new, start_graphics, done3, 
+	input Clock, enable, start, left, right, choice_for_path, win, forward, done1, done2, done1_new, done2_new, start_graphics, done3, 
 	output reg reset_signals, start_race, initial_draw_car, initial_draw_car, draw_car, draw_start_screen, draw_win_screen, move_car, plot);
+
+	output reg set_reset_signals, start_race, draw_background, draw_car, draw_over_car, draw_explosion, draw_start_screen, draw_win_screen, move, plot);
 	
-  reg[2:0] current_state, next_state;
+	reg[3:0] current_state, next_state;
 
 	
-	localparam  	
-    DRAW_GRAPHICS = 0,
-    DRAW_CAR		  = 1,
-    DRAW_BACKGROUND = 2,
-    WAIT_INPUT   = 3,
-    RE_DRAW_BACKGROUND          = 4,
-    RE_DRAW_CAR     = 5,
-    MOVE     = 6,
-    DARW_WIN_SCREEN 	  = 7,
-    WAIT_FOR_START   = 8,
-SET_RESET_SIGNAL = 9
+	localparam  	DRAW_GRAPHICS = 0,
+					SET_RESET		  = 1,
+					DRAW_BACKGROUND   = 2,
+					DRAW_CAR          = 3,
+					WAIT     = 4,
+					DARW_NEW_BACKGROUND = 5,
+					DRAW_NEW_CAR     = 6,
+					MOVE_FORWARD 	  = 7,
+					SWITCH_PASS   = 8,
+					DRAW_WIN_SCREEN	  = 9;
+					WAIT_FOR_START = 10;
     
     // Next state logic aka our state table
     always@(*)
-    begin: // state_table 
+    begin: state_table 
 		case (current_state)
 			DRAW_GRAPHICS: begin
-        if(start_graphics) begin
-					next_state = DRAW_BACKGROUND;
+				if(begin_game)begin
+					if(start_graphics) next_state = DRAW_BACKGROUND;
+					else if(start) next_state = SET_RESET;
+					else next_state = DRAW_START_SCREEN;
 				end
-				else next_state = DRAW_GRAPHICS;
 			end
+			SET_RESET: next_state = DRAW_GRAPHICS;
 			DRAW_BACKGROUND: next_state = done1 ? DRAW_CAR : DRAW_BACKGROUND;
 			DRAW_CAR: begin
-        if(done2) begin
+				if(done1) begin
 					if(win) next_state = DRAW_WIN_SCREEN;
-          else if(move == 1'b1 && Enable1Frame) next_state = RE_DRAW_BACKGROUND;
-					else next_state = WAIT_INPUT;
+					else if(forward == 1'b1 && enable) next_state = DRAW_NEW_BACKGROUND;
+					else if(left == 1'b1 || right == 1'b1) next_state = DRAW_NEW_BACKGROUND;
+					else if(start) next_state = SET_RESET;
+					else next_state = WAIT;
 				end
 				else next_state = DRAW_CAR;
 			end
-			WAIT_INPUT: begin
-        if(move == 1'b1 && Enable1Frame) next_state = RE_DRAW_BACKGROUND;
-				else next_state = WAIT_INPUT;
+			WAIT: begin
+				if(forward == 1'b1 && enable) next_state = DRAW_NEW_BACKGROUND;
+				else if(left == 1'b1 || right == 1'b1) next_state = DRAW_NEW_BACKGROUND;
+				else if(start) next_state = SET_RESET;
+				else next_state = WAIT;
 			end
-			RE_DRAW_CAR: begin
-        if(done2_new) begin
-          if(move == 1'b1) next_state = MOVE_FORWARD;
-					else if(left == 1'b1 || right == 1'b1) next_state = RE_DRAW_CAR;
+			DRAW_NEW_BACKGROUND: begin
+				if(done1_new) begin
+					if(forward == 1'b1) next_state = DRAW_NEW_CAR;
+					else if(left == 1'b1 || right == 1'b1) next_state = LEFT_RIGHT_PATH;
+					else if(start) next_state = SET_RESET;
+					else next_state = DRAW_CAR;
+				end
+				else next_state = DRAW_OVER_CAR;
+			end
+			DRAW_NEW_CAR: begin
+				if(done2_new) begin
+					if(forward == 1'b1) next_state = MOVE_FORWARD;
+					else if(left == 1'b1 || right == 1'b1) next_state = LEFT_RIGHT_PATH;
+					else if(start) next_state = SET_RESET;
 					else next_state = DRAW_CAR;
 				end
 				else next_state = DRAW_OVER_CAR;
 			end
 			MOVE_FORWARD: next_state = DRAW_CAR;
-			MOVE_LEFT_RIGHT: next_state = DRAW_CAR;
-			WAIT_LEFT_RIGHT: next_state = (left == 1'b1 || right == 1'b1) ? WAIT_LEFT_RIGHT : WAIT_FOR_MOVE;
-			DRAW_EXPLOSION: begin
-				if(DoneDrawExplosion) begin
-					if(start) next_state = DRAW_EXPLOSION;
-					else next_state = SET_RESET_SIGNALS;
-				end
-				else next_state = DRAW_EXPLOSION;
-			end
+			LEFT_RIGHT_PATH: next_state = DRAW_CAR;
 			DRAW_WIN_SCREEN: begin
-				if(DoneDrawWinScreen) begin
-					if(start) next_state = DRAW_WIN_SCREEN;
-					else next_state = SET_RESET_SIGNALS;
+				if(done3) begin
+					if(start) next_state = DRAW_BACKGROUND;
+					else next_state = WAIT_FOR_START;
 				end
 				else next_state = DRAW_WIN_SCREEN;
 			end
+			WAIT_FOR_START: next_state = start ? DRAW_BACKGROUND : WAIT_FOR_START;
 			default: next_state = SET_RESET_SIGNALS;
 		endcase
     end // state_table
@@ -74,61 +85,64 @@ SET_RESET_SIGNAL = 9
     begin: enable_signals
 	 
 		set_reset_signals = 1'b0;
-		start_race = 1'b0;
 		draw_background = 1'b0;
 		draw_car = 1'b0;
-		draw_over_car = 1'b0;
-		draw_explosion = 1'b0;
-		draw_start_screen = 1'b0;
+	        draw_new_background = 1'b0;
+		draw_new_car = 1'b0;
 		draw_win_screen = 1'b0;
 		move = 1'b0;
 		plot = 1'b0;
 	 
         case (current_state)
 			DRAW_GRAPHICS: begin
-				start_graphics = 1'b1;
+				draw_graphics = 1'b1;
 				plot = 1'b1;
 			end
-			SET_RESET_SIGNALS: reset_signals = 1'b1;
+			SET_RESET: set_reset_signals = 1'b1;
 			DRAW_BACKGROUND: begin
-				draw_background = 1'b1;
-				plot = 1'b1;
+				if(done1) plot = 1'b0;
+				else begin
+					draw_background = 1'b1;
+					plot = 1'b1;
+				end
 			end
-			START_RACE: start_race = 1'b1;
 			DRAW_CAR: begin
-				if(DoneDrawCar) plot = 1'b0;
+				if(done2) plot = 1'b0;
 				else begin
 					draw_car = 1'b1;
 					plot = 1'b1;
 				end
 			end
-			DRAW_OVER_CAR: begin
-				if(DoneDrawOverCar) plot = 1'b0;
+			DRAW_NEW_BACKGROUND: begin
+				if(done1_new) plot = 1'b0;
 				else begin
-					draw_over_car = 1'b1;
+					draw_new_background = 1'b1;
+					plot = 1'b1;
+				end
+			end
+			DRAW_NEW_CAR: begin
+				if(done2_new) plot = 1'b0;
+				else begin
+					draw_new_car = 1'b1;
 					plot = 1'b1;
 				end
 			end
 			MOVE_FORWARD: move = 1'b1;
-			MOVE_LEFT_RIGHT: move = 1'b1;
-			DRAW_EXPLOSION: begin
-				if(DoneDrawExplosion) plot = 1'b0;
-				else begin
-					draw_explosion = 1'b1;
-					plot = 1'b1;
-				end
-			end
+			LEFT_RIGHT_PATH: move = 1'b1;
 			DRAW_WIN_SCREEN: begin
 				draw_win_screen = 1'b1;
 				plot = 1'b1;
+			end
+			WAIT_FOR_START: begin
+				if(start) draw_new_background = 1'b1;
 			end
         endcase
     end // enable_signals
    
     // current_state registers
     always@(posedge Clock)
-    begin: state_FFs
-        if(!Resetn)
+    begin: 
+	    if(!start)
            current_state <= SET_RESET_SIGNALS;
         else
             current_state <= next_state;
